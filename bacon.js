f72f350ad376a486f2f0204cd54b637bef748055
Bryan Woods <bryanwoods4e@gmail.com>
2010-05-17 11:55:34 -0400

    Initial commit

---
+++ bacon.js
@@ -0,0 +1,40 @@
+require.paths.unshift('express/lib')
+require('express')
+require('express/plugins')
+
+configure(function() {
+  set('root', __dirname)
+  use(Logger)
+  use(Static, {
+    path: require("path").join(__dirname, "public")
+  }) 
+})
+
+get('/', function() {
+  this.redirect('/chunky/bacon')
+})
+
+get('/*.css', function(file){
+  this.render(file + '.css.sass', {
+    layout: false
+  })
+})
+
+get('/public/*', function(file){ 
+  this.sendfile(__dirname + '/public/' + file) 
+}) 
+
+
+get ('/chunky/bacon', function() {
+  var mime = require('express/mime')
+  this.render('bacon.html.haml', {
+    layout: false,
+    locals: {
+      title: 'Chunky Bacon!',
+      logo: '/public/images/bacon.jpg'
+    }
+  })
+})
+
+run()
+
